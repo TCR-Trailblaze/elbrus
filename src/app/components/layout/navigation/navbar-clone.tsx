4e7e69e61521295ecad0b9d44a04e343324ddc97
albarmo <moerhamsa@gmail.com>
2023-12-02 00:58:07 +0700

    feat:responsive-recruitment-page

--- src/app/components/layout/navigation/navbar-clone.tsx
+++ src/app/components/layout/navigation/navbar-clone.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import DrawerComponent from "./Drawer";
 import useModalDisclosure from "../../helpers/hooks/useModalDisclosure";
@@ -8,23 +8,23 @@ const Navbar = () => {
     // cara pakenya
     // variable isOpen =  flaging (boolean) untuk buka tutup drawer
     // func open  =  buka drawer ex: open()
-    // func close =  tutup drawer ex: close() 
-    const { isOpen, open, close } = useModalDisclosure()
+    // func close =  tutup drawer ex: close()
+    const { isOpen, open, close } = useModalDisclosure();
 
-    const [currentSection, setCurrentSection] = useState<string>('/')
-    console.log(currentSection, isOpen)
+    const [currentSection, setCurrentSection] = useState<string>("/");
+    console.log(currentSection, isOpen);
 
     // #ini untuk handle click outside (diluar wrapper drawer) biar ke close nge
     const handleClickOutside = (event: MouseEvent) => {
         const target = event.target as HTMLElement;
-        if (!target.closest('#drawer-navigation')) {
-            close()
+        if (!target.closest("#drawer-navigation")) {
+            close();
         }
     };
     useEffect(() => {
-        document.addEventListener('click', handleClickOutside, true);
+        document.addEventListener("click", handleClickOutside, true);
         return () => {
-            document.removeEventListener('click', handleClickOutside, true);
+            document.removeEventListener("click", handleClickOutside, true);
         };
     }, []);
 
@@ -32,10 +32,35 @@ const Navbar = () => {
         <header className="fixed top-0 w-full h-14 bg-white z-[100]">
             {/* 2.a (pemanggilan di child component). variable dan function custom hooks diprops kesini component drawer */}
             {/* CODE : 1.1 Pemanggilan navbar */}
-            <DrawerComponent isOpen={isOpen} close={close} active={""} setCurrentSection={setCurrentSection} />
+            <DrawerComponent
+                isOpen={isOpen}
+                close={close}
+                active={""}
+                setCurrentSection={setCurrentSection}
+            />
             {/* 2.b (pemanggilan langsung). disini langsung dipanggil open() */}
-            <button onClick={() => open()}>OPEN DRAWER</button>
+            {/* <button onClick={() => open()} className="md:hidden">
+                OPEN DRAWER
+            </button> */}
+            <div className="bg-red-300">
+                {" "}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                    onClick={() => open()}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                    />
+                </svg>
+            </div>
         </header>
-    )
-}
-export default Navbar
+    );
+};
+export default Navbar;
--- src/app/recruitment/page.tsx
+++ src/app/recruitment/page.tsx
@@ -2,6 +2,53 @@
 
 import Lottie from "lottie-react";
 import MountainAnimation from "../../../public/animation/mountain-lottie.json";
+import { useState } from "react";
+import Image from "next/image";
+
+const LIST = [
+    {
+        id: 0,
+        lable: "Nama Lengkap",
+        type: "text",
+        placeholder: "Masukan Nama Anda!",
+    },
+    {
+        id: 1,
+        lable: "Alamat Email ",
+        type: "text",
+        placeholder: "Masukan Email Anda!",
+    },
+    {
+        id: 2,
+        lable: "Nomor Telepon",
+        type: "number",
+        placeholder: "Masukan Nomor Telepon Anda!",
+    },
+    {
+        id: 3,
+        lable: "Tempat, Tanggal Lahir",
+        type: "text",
+        placeholder: "Masukan Tempat Tanggal Lahir Anda!",
+    },
+    {
+        id: 4,
+        lable: "Alamat",
+        type: "text",
+        placeholder: "Masukan Alamat Anda!",
+    },
+    {
+        id: 5,
+        lable: "Program Studi",
+        type: "text",
+        placeholder: "Masukan Prodi Anda!",
+    },
+    {
+        id: 6,
+        lable: "Tahun Angkatan",
+        type: "number",
+        placeholder: "Masukan Tahun Angkatan Anda!",
+    },
+];
 
 const Recruitment = () => {
     return (
@@ -23,50 +70,230 @@ const Recruitment = () => {
             </div>
             <div className="bg-white w-full absolute top-1/3  md:top-2/4 min-h-screen z-10 p-5 md:p-40 md:py-10 overflow-hidden space-y-3">
                 <h2 className="font-semibold text-2xl">Recruitment Form</h2>
-                <div className="border-solid border-[1px] border-gray p-5 rounded">
-                    <h1 className="text-xl">Talent Information</h1>
-                    <div className="flex flex-row p-10 space-x-5">
-                        <div className="bg-red-300 basis-96 space-y-7">
-                            <h1 className="text-lg font-medium">
-                                Name
-                                <span className="bg-[#E2E8F0] text-[#94A3B8] rounded text-sm ml-3">
-                                    Required
-                                </span>
-                            </h1>
-                            <h1 className="text-lg font-medium">Email</h1>
-                            <h1 className="text-lg font-medium">
-                                Phone Number
-                            </h1>
-                            <h1 className="text-lg font-medium">
-                                Place & Date of Birth
-                            </h1>
-                            <h1 className="text-lg font-medium">
-                                Study Program
-                            </h1>
-                            <h1 className="text-lg font-medium">
-                                Home Address
-                            </h1>
-                            <div>
-                                <h1 className="text-lg font-medium">
-                                    Reason for Joining Tricora
-                                </h1>
-                                <h1 className="text-lg font-medium">
-                                    History of Disease
-                                </h1>
-                                <h1 className="text-lg font-medium">
-                                    Outdoor Experience
-                                </h1>
-                                <h1 className="text-lg font-medium">
-                                    Passport Photo
-                                </h1>
+
+                {/* media screen for LG - XL */}
+
+                <div className="hidden lg:flex flex-col space-y-5">
+                    {/* TALENT INFORMATION */}
+                    <form
+                        action=""
+                        className=" border-solid border-[1px] border-[#E2E8F0] rounded p-5 space-y-7"
+                    >
+                        <h1 className="text-xl font-medium">
+                            Talent Information
+                        </h1>
+                        {LIST.map((input, index) => (
+                            <div
+                                key={index}
+                                className="flex justify-between mx-20 pt-3 "
+                            >
+                                <label htmlFor="name" className="text-lg">
+                                    {input.lable}
+                                </label>
+                                <input
+                                    id="name"
+                                    type={input.type}
+                                    placeholder={input.placeholder}
+                                    className="border-solid border-[1px] rounded p-3 border-slate-500 w-10/12"
+                                />
+                            </div>
+                        ))}
+                    </form>
+
+                    {/* OTHER INFORMATION */}
+                    <form
+                        action=""
+                        className=" border-solid border-[1px] border-[#E2E8F0] rounded p-5 space-y-7"
+                    >
+                        <h1 className="text-xl font-medium">
+                            Other Information
+                        </h1>
+                        <div className="flex justify-between mx-20 pt-3 ">
+                            <label className="text-lg">
+                                Alasan Masuk Tricora
+                            </label>
+                            <textarea
+                                placeholder="Masukan Alasan Anda!"
+                                name="postContent"
+                                rows={5}
+                                cols={50}
+                                className="border-solid border-[1px] rounded p-3 h-30 border-slate-500 w-10/12"
+                            />
+                        </div>
+                        <div className="flex justify-between mx-20 pt-3 ">
+                            <label className="text-lg">
+                                Pengalaman Kegiatan Outdoor
+                            </label>
+                            <textarea
+                                placeholder="Ceritakan Pengalaman Anda!"
+                                name="postContent"
+                                rows={5}
+                                cols={50}
+                                className="border-solid border-[1px] rounded p-3 h-30 border-slate-500 w-10/12"
+                            />
+                        </div>
+                        <div className="flex justify-between mx-20 pt-3 ">
+                            <label className="text-lg">Riwayat Penyakit</label>
+                            <div className="flex flex-col pl-3 h-30 w-10/12 space-y-5">
+                                <div className="space-x-6">
+                                    <input type="checkbox" className="pt-2" />
+                                    Penyakit jantung
+                                    <input type="checkbox" className="pt-2" />
+                                    Alergi
+                                    <input type="checkbox" className="pt-2" />
+                                    Asma
+                                    <input type="checkbox" className="pt-2" />
+                                    Riwayat patah tulang
+                                </div>
+                                <textarea
+                                    placeholder="Atau Penyakit Yang Lainnya!"
+                                    name="postContent"
+                                    rows={5}
+                                    cols={50}
+                                    className="border-solid border-[1px] rounded p-3 h-30 border-slate-500 w-10/12"
+                                />
                             </div>
                         </div>
 
-                        <div className="bg-blue-300 basis-5/6">
-                            <form action="">
-                                <input type="name" className="border-solid border-[1px] border-slate rounded w-full" />
-                            </form>
+                        <div className="flex justify-between mx-20 pt-3 ">
+                            <label className="text-lg">Upload Pass Photo</label>
+                            <div className=" p-3 h-30 border-slate-500 w-5/6 h-44 ">
+                                <section className="w-96 h-full border-dashed border-2 border-slate-500 flex flex-col justify-center items-center  rounded-md">
+                                    <Image
+                                        src={"/images/file.png"}
+                                        alt="file-photo"
+                                        width={100}
+                                        height={20}
+                                        className=" h-24"
+                                    />
+                                    <input
+                                        type="file"
+                                        className="text-sm cursor-pointer object-cover"
+                                    />
+                                </section>
+                            </div>
                         </div>
+                    </form>
+                    <div className="flex justify-end space-x-5">
+                        <button className="bg-white hover:bg-gray-300 text-[#64748B] border-solid border-[1px] border-black rounded h-10 w-24">
+                            Discard
+                        </button>
+                        <button className="bg-[#1E68D7] text-white rounded h-10 w-24 hover:bg-blue-900">
+                            Update
+                        </button>
+                    </div>
+                </div>
+
+                {/* media screen for XS - MD */}
+                <div className="lg:hidden space-y-5">
+                    {/* TALENT INFORMATION*/}
+                    <form
+                        action=""
+                        className=" border-solid border-[1px] border-[#E2E8F0] rounded p-2 space-y-5"
+                    >
+                        <h1 className="text-xl font-medium">
+                            Talent Information
+                        </h1>
+                        {LIST.map((input, index) => (
+                            <div
+                                key={index}
+                                className="flex justify-between p-2 py-3 space-x-5 "
+                            >
+                                <label
+                                    htmlFor="name"
+                                    className="text-base w-32 flex flex-col justify-center"
+                                >
+                                    {input.lable}
+                                </label>
+                                <input
+                                    id="name"
+                                    type={input.type}
+                                    placeholder={input.placeholder}
+                                    className="border-solid border-[1px] rounded p-3 border-slate-500 w-8/12"
+                                />
+                            </div>
+                        ))}
+                    </form>
+
+                    {/* OTHER INFORMATION */}
+                    <form
+                        action=""
+                        className=" border-solid border-[1px] border-[#E2E8F0] rounded p-2 space-y-3"
+                    >
+                        <h1 className="text-xl font-medium">
+                            Other Information
+                        </h1>
+                        <div className="flex justify-between p-2 space-x-5">
+                            <label className="text-base flex items-center w-32">
+                                Alasan Masuk Tricora
+                            </label>
+                            <textarea
+                                placeholder="Masukan Alasan Anda!"
+                                name="postContent"
+                                rows={5}
+                                cols={50}
+                                className="border-solid border-[1px] rounded p-3 h-30 border-slate-500 w-8/12"
+                            />
+                        </div>
+                        <div className="flex justify-between p-2 space-x-5">
+                            <label className="text-base flex items-center w-32">
+                                Pengalaman Berkegiatan Outdoor
+                            </label>
+                            <textarea
+                                placeholder="Masukan Alasan Anda!"
+                                name="postContent"
+                                rows={5}
+                                cols={50}
+                                className="border-solid border-[1px] rounded p-3 h-30 border-slate-500 w-8/12"
+                            />
+                        </div>
+
+                        <div className=" flex justify-between  space-x-5">
+                            <label className="text-base flex items-center p-2 w-32">
+                                Riwayat Penyakit
+                            </label>
+                            <div className="flex flex-col p-3 h-30 w-8/12 space-y-3 ">
+                                <div className="space-x-3 text-sm  w-50">
+                                    <input type="checkbox" className="pt-2" />
+                                    Penyakit jantung
+                                    <input type="checkbox" className="pt-2" />
+                                    Alergi
+                                    <input type="checkbox" className="pt-2" />
+                                    Asma
+                                    <input type="checkbox" className="pt-2" />
+                                    Riwayat patah tulang
+                                </div>
+                                <textarea
+                                    placeholder="Atau Penyakit Yang Lainnya!"
+                                    name="postContent"
+                                    rows={5}
+                                    cols={50}
+                                    className="border-solid border-[1px] rounded p-3 h-30 flex justify-end border-slate-500 "
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between p-2 space-x-5 ">
+                            <label className="text-base flex items-center w-32">
+                                Upload Pass Photo
+                            </label>
+                            <div className="flex justify-start w-8/12 p-2 h-40">
+                                <section className="border-slate-500 w-64 p-2 flex flex-col justify-end h-full border-dashed border-2  rounded-md">
+                                    <input
+                                        type="file"
+                                        className="text-sm cursor-pointer object-cover"
+                                    />
+                                </section>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="flex justify-end space-x-4">
+                        <button className="bg-white hover:bg-gray-300 text-[#64748B] border-solid border-[1px] border-black rounded h-10 w-24">
+                            Discard
+                        </button>
+                        <button className="bg-[#1E68D7] text-white rounded h-10 w-24 hover:bg-blue-900">
+                            Update
+                        </button>
                     </div>
                 </div>
             </div>
